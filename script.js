af22e61d2597678d8e5835a4872193b5e243f09f
aayu1724 <aayushtest17@gmail.com>
2026-05-01 01:42:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+let current = 0;
+const screens = document.querySelectorAll(".screen");
+
+/* SHOW SCREEN */
+function showScreen(index) {
+    screens[current].classList.remove("active");
+    current = index;
+    screens[current].classList.add("active");
 }
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+
+/* SWIPE */
+let startX = 0;
+
+document.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX;
+});
+
+document.addEventListener("touchend", (e) => {
+    let endX = e.changedTouches[0].clientX;
+    let diff = startX - endX;
+
+    if (diff > 50 && current < screens.length - 1) {
+        showScreen(current + 1);
+    }
+
+    if (diff < -50 && current > 0) {
+        showScreen(current - 1);
+    }
+});
+
+/* ESCAPE BUTTON */
+const btn = document.querySelector(".reject");
+
+btn.addEventListener("mouseenter", move);
+btn.addEventListener("touchstart", move);
+
+function move() {
+    btn.style.position = "fixed";
+    btn.style.left = Math.random() * (window.innerWidth - 100) + "px";
+    btn.style.top = Math.random() * (window.innerHeight - 50) + "px";
+}
